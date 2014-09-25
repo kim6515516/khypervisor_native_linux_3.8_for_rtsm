a4b5554b230498798ee252991707d72335d1e294
kim6515516 <kim6515516@kim6515516-All-Series>
2014-09-25 13:27:27 +0900

    init files

---
+++ include/generated/compile.h
@@ -0,0 +1,6 @@
+/* This file is auto generated, version 2 */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 Wed Sep 24 15:32:57 KST 2014"
+#define LINUX_COMPILE_BY "kim6515516"
+#define LINUX_COMPILE_HOST "kim6515516-All-Series"
+#define LINUX_COMPILER "gcc version 4.6.3 (Ubuntu/Linaro 4.6.3-1ubuntu5) "
